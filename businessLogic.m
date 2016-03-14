c8e36731580b8cf9defb94389f94cfaa39bd6ba0
di07zd4 <charles.walker@tn.gov>
2016-03-14 11:38:24 -0500

    “commit

---
+++ businessLogic.m
@@ -0,0 +1,14 @@
+//
+//  businessLogic.m
+//  gitEx11
+//
+//  Created by di07zd4 on 3/14/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+
+// this is a comment to track
+
+// this is another comment (to track)
---
+++ main.m
@@ -0,0 +1,26 @@
+//
+//  main.m
+//  gitEx11
+//
+//  Created by di07zd4 on 3/14/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World from Eric!");
+        
+        NSLog(@"this is the 2nd log line written from main()" );
+        
+        NSLog(@"this is the 3rd log line written from main()" );
+        
+    }
+    return 0;
+    
+    
+    //  this is a comment in the main() function
+    
+}
